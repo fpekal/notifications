98fa9a8f6e4336752c04d3957e4355bd9193ee1a
Filip Pękala <272985@student.pwr.edu.pl>
2024-12-15 22:56:17 +0100

    Added top-level flake

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+	inputs = {
+		nixpkgs.url = "github:NixOS/nixpkgs";
+		server = {
+			url = "./server";
+			inputs.nixpkgs.follows = "nixpkgs";
+		};
+
+		client = {
+			url = "./client";
+			inputs.nixpkgs.follows = "nixpkgs";
+		};
+	};
+
+	outputs =
+	{ nixpkgs, server, client, self }:
+	{
+		packages.x86_64-linux = {
+			notifications-server = server.packages.x86_64-linux.default;
+			notifications-client = client.packages.x86_64-linux.default;
+		};
+
+		overlays.default = final: prev:
+		{
+			notifications-server = self.packages.x86_64-linux.notifications-server;
+			notifications-client = self.packages.x86_64-linux.notifications-client;
+		};
+	};
+}
